8715763cd104bf65d58c7b1a187bff8402addab0
globalpolaris <54710635+globalpolaris@users.noreply.github.com>
2021-09-06 11:27:54 +0700

    fix sign up

--- client/src/adapters/SignupHandler.js
+++ client/src/adapters/SignupHandler.js
@@ -1,15 +1,15 @@
 import instance from "./AxiosInstance";
 
-const SignupHandler =async (user) => {
+const SignupHandler = async (user) => {
   try {
     const data = await instance.post(`user/register`, {
       username: user.username,
       email: user.email,
       password: user.password,
     });
-    return data.status
+    return data.status;
   } catch (error) {
-    return error.response.status
+    return error.response.status;
   }
 };
 
--- client/src/components/hooks/useForm.js
+++ client/src/components/hooks/useForm.js
@@ -10,9 +10,11 @@ const useForm = (callback, validate) => {
   });
   const [errors, setErrors] = useState({});
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isValid, setIsValid] = useState(false);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
+
     setValues({
       ...values,
       [name]: value,
@@ -21,9 +23,17 @@ const useForm = (callback, validate) => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    console.log("ter");
+
+    setErrors(validate(values));
     const response = await SignupHandler(values);
+    console.log(errors);
+    console.log(response);
     setErrors(validate(values, response));
-    setIsSubmitting(true);
+    if (response === 201) {
+      setIsSubmitting(true);
+    }
   };
 
   useEffect(() => {
