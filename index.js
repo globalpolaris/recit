1893f7506da2188f1a1f8494f40bbd4ccb03c821
globalpolaris <54710635+globalpolaris@users.noreply.github.com>
2021-09-07 11:08:10 +0700

    deploy setup

--- index.js
+++ index.js
@@ -1,46 +1,52 @@
-require('dotenv').config()
-const express = require('express');
-const journalRouter = require('./routers/journal');
-const userRouter = require('./routers/users');
-const cors = require('cors');
-const {DB_HOST, DB_PORT, DB} = process.env
+require("dotenv").config();
+const express = require("express");
+const journalRouter = require("./routers/journal");
+const userRouter = require("./routers/users");
+const path = require("path");
+const cors = require("cors");
+const { MONGODB_CONNECTION_STRING } = process.env;
 
 const app = express();
-const port = 3002;
+const port = process.env.PORT || 3002;
 
 //GET, PUT, POST, DELETE
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: "http://localhost:3000",
 };
 
-const mongoose = require('mongoose');
-console.log(DB_HOST, DB_PORT, DB)
-mongoose.connect(`mongodb://${DB_HOST}:${DB_PORT}/${DB}`, {
+const mongoose = require("mongoose");
+console.log(DB_HOST, DB_PORT, DB);
+mongoose.connect(MONGODB_CONNECTION_STRING, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
 });
 
 const db = mongoose.connection;
-db.on('error', () => {
-  console.log('Failed to connect to database');
+db.on("error", () => {
+  console.log("Failed to connect to database");
 });
 
-db.once('open', () => {
-  console.log('Successfully connected to database');
+db.once("open", () => {
+  console.log("Successfully connected to database");
 });
 
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/assets', express.static('public'));
-app.set('view engine', 'ejs');
+app.use("/assets", express.static("public"));
+app.set("view engine", "ejs");
 app.use(journalRouter);
 app.use(userRouter);
+
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 
 // app.get('/', (req, res) => {
 //   const responseText = `Requested at: ${req.myDate.toString()}`;
 //   res.render('./pages/index', { responseText: responseText });
 // });
 
-app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
+app.listen(port, () => console.log(`Listening on ${port}`));
