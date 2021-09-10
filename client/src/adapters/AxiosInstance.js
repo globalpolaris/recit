63aad1d7b6c5ac664f0df74e364a8b7365721f21
globalpolaris <54710635+globalpolaris@users.noreply.github.com>
2021-09-10 10:46:05 +0700

    Update AxiosInstance.js

--- client/src/adapters/AxiosInstance.js
+++ client/src/adapters/AxiosInstance.js
@@ -3,7 +3,7 @@ const axios = require("axios").default;
 console.log(process.env.PORT);
 const baseUrl = process.env.baseURL || "http://localhost:3002";
 const instance = axios.create({
-  baseURL: `${baseUrl}/api/`,
+  baseURL: `https://recit-note-app.herokuapp.com/api/`,
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
